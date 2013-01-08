8635aa28128f467e43051c8cc2ce8c77ca23a395
Neung <neung@Neungs-MacBook-Pro.local>
2013-01-08 12:42:51 +0700

    Create person's hash

--- learningruby.rb
+++ learningruby.rb
@@ -1,5 +1,11 @@
 animals = ['Cat','Dog','Iguana', nil]
 
+person = {
+	name: "Neung",
+	email: "eakpun@gmail.com",
+	colors: ["RED","GREEN","BLUE"]
+}
+
 animals.each do |animal|
 	if(animal != nil) 
 	puts "I have #{animal}"
