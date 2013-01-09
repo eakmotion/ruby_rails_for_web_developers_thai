1547926cb9f4e21951a031fdebb47f2da621553d
eakmotion <eakpun@gmail.com>
2013-01-09 10:52:08 +0700

    Explain result from Golden retriver

--- learningruby.rb
+++ learningruby.rb
@@ -8,30 +8,48 @@ person = {
 
 class Animal 
 	attr_accessor :name , :age , :gender
-	def initialize
-		@name = "Dog"
-		@age = "3"
-		@gender = "Male"
+	def initialize(option={})
+		self.name = option[:name] || "Snoopy"
+		self.age = option[:age] || 1
+		self.gender = option[:gender] || "male"
 	end
 end
 
 class Mammal < Animal
 	attr_accessor :legs , :baby_food , :special_ability
-	def initialize
+	def initialize(option={})
 		super
-		@legs = "4"
-		@baby_food = "Milk"
-		@special_ability = nil
+		self.legs = option[:legs] || 4
+		self.baby_food = option[:baby_food] || "milk"
+		self.special_ability = option[:special_ability] || nil
 	end
 
 end
 
 class Bird < Animal
-	attr_accessor :wings , :legs
-	def initialize
+	attr_accessor :wings
+	def initialize(option={})
 		super
-		@wings = "2"
-		@legs = "2"
+		self.wings = option[:wings] || 2
+		self.legs = option[:legs] || 2
+	end
+end
+
+class Dog < Mammal
+	attr_accessor :best_friend
+	def initialize(option={})
+		super
+		self.best_friend = true
+		self.special_ability = option[:special_ability] || "Friendly"
+	end
+end
+
+class Cat < Mammal
+	attr_accessor :claw_sharp
+	def initialize(option={})
+		super
+		self.claw_sharp = true
+		self.special_ability = option[:special_ability] || "climb up to the tree"
 	end
 end
 
@@ -39,3 +57,10 @@ end
 animals.compact.each do |animal|
 	puts "I have #{animal}"
 end
+
+golden_retriever = Dog.new #set golden retriver is Dog
+golden_retriever.legs #call legs result (default = 4)
+golden_retriever.best_friend #call golden retriver is best friend  
+golden_retriever.special_ability #call golden retriver special ability
+
+
