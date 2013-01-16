f17fa38881ed5519823b2c48c77edc70122f2c9d
eakmotion <eakpun@gmail.com>
2013-01-16 11:02:00 +0700

    Model Application Update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130116031828) do
+ActiveRecord::Schema.define(:version => 20130116040042) do
 
   create_table "competitors", :force => true do |t|
     t.integer  "user_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20130116031828) do
     t.integer  "match_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "rating"
   end
 
   create_table "sports", :force => true do |t|
