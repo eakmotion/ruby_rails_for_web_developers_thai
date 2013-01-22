273bc0a57f88470bac03a351d42501d4dc948ab5
eakmotion <eakpun@gmail.com>
2013-01-22 21:24:03 +0700

    Added comment feature in match[update]

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	attr_accessible :content
+	attr_accessible :content, :match_id, :parent_type
 
 	belongs_to :parent, polymorphic: true
 	belongs_to :user, inverse_of: :statuses
